bfb93c0565680f006a0e755668053d08a043d9ba
Zjuccen <Zjac.nt@gmail.com>
2021-12-07 16:06:48 +0100

    ændringer i alt

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-
 function openNav() {
-    document.getElementById("mySidenav").style.width = "175px";
-  }
-   
-  function closeNav() {
-       document.getElementById("mySidenav").style.width = "0";
-  }  
+     document.getElementById("mySidenav").style.width = "250px";
+}
+    
+function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+}
