b2bd87da360c040fee99aa040cd57b48d6f52f8a
Zjuccen <Zjac.nt@gmail.com>
2021-12-07 19:40:10 +0100

    Begyndt at lave på sections

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Burgermenu åben og luk //
+
 function openNav() {
      document.getElementById("mySidenav").style.width = "90%";
 }
@@ -5,3 +7,33 @@ function openNav() {
 function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
 }
+
+
+
+
+//Scroll too funktionen //
+
+$(document).ready(function(){
+     // Add smooth scrolling to all links
+     $("a").on('click', function(event) {
+   
+       // Make sure this.hash has a value before overriding default behavior
+       if (this.hash !== "") {
+         // Prevent default anchor click behavior
+         event.preventDefault();
+   
+         // Store hash
+         var hash = this.hash;
+   
+         // Using jQuery's animate() method to add smooth page scroll
+         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+         $('html, body').animate({
+           scrollTop: $(hash).offset().top
+         }, 800, function(){
+      
+           // Add hash (#) to URL when done scrolling (default click behavior)
+           window.location.hash = hash;
+         });
+       } // End if
+     });
+   });
