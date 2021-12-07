a7f26943290fec17c975c9e2ee1659cdde5ccfa3
Zjuccen <Zjac.nt@gmail.com>
2021-12-07 14:44:29 +0100

    Start på portfolie

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "175px";
+  }
+   
+  function closeNav() {
+       document.getElementById("mySidenav").style.width = "0";
+  }  
